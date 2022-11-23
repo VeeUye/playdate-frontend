c5da45d4797bc340ab40a77207ca822089f7e4a6
VeeUye <97988558+VeeUye@users.noreply.github.com>
2022-11-23 22:58:35 +0000

    test: initial test setup for MultiSelect

--- src/components/atoms/form-input/MultiSelectInput.test.js
+++ src/components/atoms/form-input/MultiSelectInput.test.js
@@ -3,7 +3,27 @@ import MultiSelectInput from "./MultiSelectInput";
 import { render, screen } from "@testing-library/react";
 
 describe("MultiSelectInput", () => {
-  it("displays the user's current friends", () => {
-    render(<MultiSelectInput />);
+  const setUp = (props) => {
+    const initialProps = {
+      name: "multi-select",
+      label: "Invite",
+      options: [
+        { value: "FaoLwxoE2ub6qYMZRACNiNEth9OH", label: "Chief Wiggum" },
+        { value: "t0fvq3bA77Z7X3F1a7LkeCOBipne", label: "Ned Flanders" },
+      ],
+      placeholder: "Select...",
+      onChange: jest.fn(),
+    };
+
+    const combinedProps = {
+      initialProps,
+      ...props,
+    };
+
+    render(<MultiSelectInput {...combinedProps} />);
+  };
+
+  it("displays the users current invitable friends", () => {
+    setUp();
   });
 });
