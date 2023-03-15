25f6383e9ee8ebcc2018219bb9474821344a5778
VeeUye <97988558+VeeUye@users.noreply.github.com>
2023-03-15 21:23:32 +0000

    test: extend test coverage for CreateProfileForm

--- src/components/organisms/CreateProfileForm/index.test.js
+++ src/components/organisms/CreateProfileForm/index.test.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { useHistory, MemoryRouter } from "react-router-dom";
 import MockAdapter from "axios-mock-adapter";
-import { MemoryRouter } from "react-router-dom";
-import { useHistory } from "react-router-dom";
-import CreateEventForm from "./index";
 import CreateProfileForm from "./index";
 
 const axios = require("axios");
@@ -33,14 +31,12 @@ describe("CreateProfileForm", () => {
   });
 
   const stubbedFields = {
-    name: "A lovely event name",
-    description: "A lovely description",
-    date_start: "2024-02-01T11:00",
-    date_end: "2024-02-02T12:00",
-    location: "A lovely location",
-    friends_invited: ["Chief Wiggum", "Luann Van Houten"],
-    owner: 123,
-    friends_accepted: [123],
+    name: "Crabbo",
+    description: "I'm a little crabbo",
+    children: ["Maggie", "Lisa", "Bart"],
+    location: "Springfield",
+    friends: ["firstFriend"],
+    userId: 123,
   };
 
   const setup = (props) => {
@@ -63,39 +59,58 @@ describe("CreateProfileForm", () => {
     );
   };
 
+  const nameField = () => screen.getByLabelText("Your Name");
+  const aboutField = () => screen.getByLabelText("About You");
+  const childrenField = () => screen.getByLabelText("Child/Children's name");
+  const locationField = () => screen.getByLabelText("Location");
+
   it("updates the name field with the correct user input", () => {
     setup();
-    const nameField = screen.getByLabelText("Your Name");
 
-    fireEvent.change(nameField, { target: { value: "Crabbo" } });
+    fireEvent.change(nameField(), { target: { value: "Crabbo" } });
 
-    expect(nameField.value).toContain("Crabbo");
+    expect(nameField().value).toContain("Crabbo");
   });
 
   it("updates the description field with the correct user input", () => {
     setup();
 
-    const aboutField = screen.getByLabelText("About You");
-
-    fireEvent.change(aboutField, {
+    fireEvent.change(aboutField(), {
       target: { value: "I'm a little crabbo" },
     });
 
-    expect(aboutField.value).toContain("I'm a little crabbo");
+    expect(aboutField().value).toContain("I'm a little crabbo");
+  });
+
+  it("updates the children field with the correct user input", () => {
+    setup();
+
+    fireEvent.change(childrenField(), {
+      target: { value: "Maggie, Lisa, Bart" },
+    });
+
+    expect(childrenField().value).toContain("Maggie, Lisa, Bart");
   });
 
   it("updates the location field with the correct user input", () => {
     setup();
 
-    const locationField = screen.getByLabelText("Location");
+    fireEvent.change(locationField(), { target: { value: "Springfield" } });
 
-    fireEvent.change(locationField, { target: { value: "Springfield" } });
-
-    expect(locationField.value).toContain("Springfield");
+    expect(locationField().value).toContain("Springfield");
   });
 
   it("submits the form when the the Create Profile button is clicked and redirects to the profile page", async () => {
     setup();
+
+    fireEvent.change(nameField(), { target: { value: "Crabbo" } });
+    fireEvent.change(aboutField(), {
+      target: { value: "I'm a little crabbo" },
+    });
+    fireEvent.change(childrenField(), {
+      target: { value: "Maggie, Lisa, Bart" },
+    });
+    fireEvent.change(locationField(), { target: { value: "Springfield" } });
 
     const button = screen.getByRole("button", { name: "Create Profile" });
 
@@ -106,33 +121,19 @@ describe("CreateProfileForm", () => {
     });
   });
 
-  xit("it calls the post event endpoint with the correct payload", async () => {
+  it("it calls the post event endpoint with the correct payload", async () => {
     setup();
 
-    const button = screen.getByRole("button", { name: "Create Event" });
+    const button = screen.getByRole("button", { name: "Create Profile" });
 
-    fireEvent.change(nameField(), { target: { value: stubbedFields.name } });
-    fireEvent.change(descriptionField(), {
-      target: { value: stubbedFields.description },
+    fireEvent.change(nameField(), { target: { value: "Crabbo" } });
+    fireEvent.change(aboutField(), {
+      target: { value: "I'm a little crabbo" },
     });
-    fireEvent.change(startDateField(), {
-      target: { value: stubbedFields.date_start },
+    fireEvent.change(childrenField(), {
+      target: { value: "Maggie, Lisa, Bart" },
     });
-    fireEvent.change(endDateField(), {
-      target: { value: stubbedFields.date_end },
-    });
-    fireEvent.change(locationField(), {
-      target: { value: stubbedFields.location },
-    });
-
-    const inviteSelect = screen.getByRole("combobox");
-
-    fireEvent.click(inviteSelect);
-
-    const options = screen.getAllByRole("option");
-
-    fireEvent.click(options[0]);
-    fireEvent.click(options[3]);
+    fireEvent.change(locationField(), { target: { value: "Springfield" } });
 
     fireEvent.click(button);
 
